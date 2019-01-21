2d46c9f13f808849199c82b9d0c27d63ad397ba3
唐文捷 <matonna@163.com>
2019-01-21 18:03:59 +0800

    change code

--- src/app.vue
+++ src/app.vue
@@ -8,17 +8,22 @@
 </template>
 
 <script lang="ts">
+import { someVar, Point } from "./type";
 import Vue from "vue";
 import Component from "vue-class-component";
 
 @Component
 export default class App extends Vue {
   // 初始化数据
-  msg: string = "123";
-
+  msg: string = someVar;
+  obj: Point = {
+    x: 1,
+    y: "abc"
+  };
   // 声明周期钩子
   mounted() {
     this.greet();
+    console.log(this.obj);
   }
 
   // 计算属性
---
+++ src/type.ts
@@ -0,0 +1,7 @@
+const someVar = '123';
+interface Point {
+	x: number;
+	y: string;
+}
+
+export { someVar, Point };
